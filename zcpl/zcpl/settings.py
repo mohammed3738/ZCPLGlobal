c27e005b276f991a19a8ad098be9085b62f03ff6
abhirajdas06 <abhirajdas57@gmail.com>
2025-06-28 16:02:13 +0530

    refactor static file settings in Django configuration

--- zcpl/zcpl/settings.py
+++ zcpl/zcpl/settings.py
@@ -119,16 +119,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Additional directories to look for static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Example: Custom static directory
-]
+# # Additional directories to look for static files
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # Example: Custom static directory
+# ]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ for collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # ✅ custom static dir
+
 
 LOGIN_URL = '/signin/'
 
