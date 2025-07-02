c6de3fb323f933185023568d7e4d58f5b85b1fe4
abhirajdas06 <abhirajdas57@gmail.com>
2025-07-02 17:23:21 +0530

    add slugify import to models.py for slug generation

--- zcpl/globalwebsite/models.py
+++ zcpl/globalwebsite/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from decimal import Decimal
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
 # Create your models here.
 from django.db import models
