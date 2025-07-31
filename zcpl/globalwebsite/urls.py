f48696c40415b12fc4ce0e1a5e17b0fab272e964
abhirajdas06 <abhirajdas57@gmail.com>
2025-07-31 16:05:07 +0530

    Fix duplicate path for contact-us in URL patterns

--- zcpl/globalwebsite/urls.py
+++ zcpl/globalwebsite/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('about-us/',views.about_us,name="about_us"),
     path('services/',views.services,name="services"),
     path('contact/',views.contact,name="contact"),
+    path('contact-us/',views.contact,name="contact"),
     path('itad-services/',views.itadservices,name="itad-services"),
     path('it-hardware/',views.it_hardware,name="it-hardware"),
     path('server-maintenance/',views.server_maintenance,name="server-maintenance"),
